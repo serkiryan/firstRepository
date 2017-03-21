e3d35c097846e36277626d0a46edd3648100e2ba
Seryog Kirakosyan <seryogkirakosyan@gmail.com>
2017-03-21 11:35:56 +0400

    change style.css test commit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
         <!-- Bootstrap -->
         <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
         <link rel="stylesheet" href="css/font-awesome.css">
 
 
